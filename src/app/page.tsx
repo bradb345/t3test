ea08f6ccf04f578ecd0e71c21007b3a0705a6794
bradb345 <bradbernard17@gmail.com>
2024-09-07 12:49:19 +0100

    table changes and typscript stuff

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 // import Link from "next/link";
 import {db} from '~/server/db';
 
+export const dynamic = "force-dynamic"
+
 export default async function HomePage() {
 
-  const posts = await db.query.posts.findMany();
-  console.log('posts:', posts)
-
+  const users = await db.query.user.findMany({
+    orderBy: (model, { desc }) => desc(model.id),
+  })
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-center bg-orange-300 text-white">
@@ -17,7 +19,7 @@ export default async function HomePage() {
           This is a test page to see how things work.
         </p>
 
-        {posts.map((post) => (<p className="text-lg" key={post.id}>{post.name}</p>))}
+        {users.map((user) => (<p className="text-lg" key={user.id}>{user.name}</p>))}
         
       </div>
     </main>
